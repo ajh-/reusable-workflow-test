a346ff205f8d429159defa42f4fda5a8262bc784
Aaron Howland <7330587+ajh-@users.noreply.github.com>
2025-06-28 09:43:38 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "github" {
+  owner = "ajh-"
+  app_auth {}
+}
+
 resource "null_resource" "default" {
   provisioner "local-exec" {
     command = "echo 'Hello World'"
