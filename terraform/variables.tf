9c3a1fa4e3e1ee1e2412b29a6de9115f4147d7c4
Aaron Howland <7330587+ajh-@users.noreply.github.com>
2025-06-28 14:34:30 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,8 @@
+variable "github_app_id" {
+  description = "The GitHub App id"
+  type        = string
+}
+
 variable "github_app_installation_id" {
   description = "The GitHub App installation id"
   type        = string
