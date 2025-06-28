e9bae8f0321e676d3c82f1bcaa57461f85a0aa57
Aaron Howland <7330587+ajh-@users.noreply.github.com>
2025-06-28 13:47:42 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,19 +16,19 @@ provider "github" {
 
 resource "github_actions_variable" "example_variable" {
   repository    = "reusable-workflow-test"
-  variable_name = "terraform_test_variable-a"
+  variable_name = "terraform_test_variable_a"
   value         = "this_is_a_test_value!"
 }
 
 resource "github_actions_variable" "example_variable_2" {
   repository    = "reusable-workflow-test"
-  variable_name = "terraform_test_variable-b"
+  variable_name = "terraform_test_variable_b"
   value         = "this_is_a_test_value!"
 }
 
 resource "github_actions_variable" "example_variable_3" {
   repository    = "reusable-workflow-test"
-  variable_name = "terraform_test_variable-c"
+  variable_name = "terraform_test_variable_c"
   value         = "this_is_a_test_value!"
 }
 
